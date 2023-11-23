8b9fc9e0cfb384c2b057ed2d27d93401941e55f6
thinkAmi <dev.thinkami+git@gmail.com>
2023-11-23 19:49:55 +0900

    add react router

---
+++ src/components/pages/Introduction.tsx
@@ -0,0 +1,27 @@
+import Form from "@rjsf/core";
+import validator from "@rjsf/validator-ajv8";
+import {RJSFSchema} from "@rjsf/utils";
+
+const schema: RJSFSchema = {
+  title: 'Todo',
+  type: 'object',
+  required: ['title'],
+  properties: {
+    title: { type: 'string', title: 'Title', default: 'A new task' },
+    done: { type: 'boolean', title: 'Done?', default: false },
+  }
+}
+
+const log = (type) => console.log.bind(console, type)
+
+export const Introduction = () => {
+  return (
+    <Form
+      schema={schema}
+      validator={validator}
+      onChange={log('changed')}
+      onSubmit={log('submitted')}
+      onError={log('errors')}
+    />
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Introduction} from "./components/pages/Introduction.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Introduction />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
